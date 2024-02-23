12c147741e5823d3bd4a19eecd946a3c54c5a94d
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2024-02-23 15:05:01 -0300

    feat: ensure GetUsersMongoRepository get users from Mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,16 +1,18 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
     // Método que irá retornar uma lista de usuários
-    return [
-      {
-        firstName: "JV",
-        lastName: "Gouveia",
-        email: "jv@gmail.com",
-        password: "123",
-      },
-    ];
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
