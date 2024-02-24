d236b45de6e7960269a6e003df33c94d16fb73a9
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2024-02-24 16:28:08 -0300

    feat: add DeleteUserController and its route

---
+++ src/controllers/dete-user/delete-user.ts
@@ -0,0 +1,30 @@
+import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
+import { IDeleteUserController, IDeleteUserRepository } from "./protocols";
+
+export class DeleteUserController implements IDeleteUserController {
+  constructor(private readonly deletedUserRepository: IDeleteUserRepository) {}
+  async handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>> {
+    try {
+      const id = httpRequest?.params?.id;
+      if (!id) {
+        return {
+          statusCode: 400,
+          body: "Missing user id",
+        };
+      }
+
+      const user = await this.deletedUserRepository.deleteUser(id);
+
+      return {
+        statusCode: 200,
+        body: user,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong",
+      };
+    }
+  }
+}
